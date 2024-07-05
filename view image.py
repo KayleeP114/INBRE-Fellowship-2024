e1164fd92653936384384a83c24729105ed4fc7c
KayleeP114 <kayleep114@gmail.com>
2024-07-05 12:28:54 -0700

    Delete view image.py

--- view image.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-
-# Open an image file
-image_path = '####_hydrogen_bonds_dark_1ns_20240703_202002.png'  # Replace with your image file path
-img = Image.open(image_path)
-
-# Show the image
-img.show()
