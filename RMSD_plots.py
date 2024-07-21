6db2fade7bbc8f3b6d0083b6ef8239104e59dc80
KayleeP114 <kayleep114@gmail.com>
2024-07-21 16:55:24 -0700

    Update RMSD_plots.py

--- RMSD_plots.py
+++ RMSD_plots.py
@@ -1,25 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Function to plot RMSD data
-def plot_rmsd(H_rmsd.dat, title="RMSD", x_label="Time (ns)", y_label="RMSD (nm)", 
-              line_color='blue', font_name='Times New Roman', font_size=20):
-    # Read data from file
-    data = pd.read_csv(file_path, delim_whitespace=True)
+# Files for data inputs
+rmsd1 = 'H_rmsd.dat'
+rmsd2 = 'propka_rmsd.dat'
+rmsd3 = 'pypka_rmsd.dat'
+
+# Plot the data
+plt.figure(figsize=(10, 6))
+plt.plot(time, rmsd1, label='RMSD 1', color='blue')
+plt.plot(time, rmsd2, label='RMSD 2', color='gray')
+plt.plot(time, rmsd3, label='RMSD 3', color='red')
+plt.title('RMSD', fontsize=25, fontname='Times New Roman')
+plt.xlabel('Time (ns)', fontsize=20, fontname='Times New Roman')
+plt.ylabel('RMSD (nm)', fontsize=20, fontname='Times New Roman')
     
-    # Assuming the file has two columns: time and rmsd
-    time = data.iloc[:, 0]
-    rmsd = data.iloc[:, 1]
-    
-    # Set the font properties
-    plt.rc('font', family=font_name, size=font_size)
-    
-    # Plot the data
-    plt.figure(figsize=(10, 6))
-    plt.plot(time, rmsd, color=line_color)
-    plt.title(title)
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    
-    # Show plot
-    plt.show()
+# Show plot
+plt.show()
